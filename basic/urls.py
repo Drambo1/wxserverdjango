49a2d17ad237b37232d15dd826646213222f208a
Drambo1 <chenjunfeng@swartz.cn>
2018-01-23 16:03:39 +0800

    description: 1. init app author: chenjunfeng datetime: 2018-01-23 11:04

---
+++ basic/urls.py
@@ -0,0 +1,21 @@
+# -*- coding=utf-8 -*-
+"""
+-------------------------------------------------
+   File Name :    urls
+   Description :
+   Author :       Drambo
+   date :         18-1-23
+-------------------------------------------------
+   Change Activity:
+                  18-1-23:
+-------------------------------------------------
+"""
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^service$', views.OCRService.as_view(), name='Server Service'),
+]
+
+if __name__ == '__main__':
+    pass
--- basic/views.py
+++ basic/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class wxService(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        """
+        @attentions: 获取指定事件状态
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return Response({}, status=status.HTTP_200_OK)
